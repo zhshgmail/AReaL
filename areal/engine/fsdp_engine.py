fix fsdp engine: qwen3 TP q/k norm wrapping, gradient clipping, the scale of grad norm, and sft scripts (#335)
* .

* .

* .

areal/engine/fsdp_engine.py
@@ -43,6 +43,7 @@ from areal.utils.distributed import init_custom_process_group
 from areal.utils.fsdp import (
     CPUOffloadPolicy,
     MixedPrecisionPolicy,
+    NoParallel,
     apply_fsdp2,
     fsdp2_clip_grad_norm,
     fsdp2_load_full_state_dict,
@@ -50,7 +51,6 @@ from areal.utils.fsdp import (
 from areal.utils.model import VALID_VISION_MODELS
 from areal.utils.save_load import get_state_dict_from_repo_id_or_path
 from areal.utils.ulysses import (
-    get_ulysses_sequence_parallel_group,
     set_ulysses_sequence_parallel_group,
     ulysses_pad,
     ulysses_pad_and_slice_inputs,
@@ -153,17 +153,6 @@ class FSDPEngine(BaseHFEngine):
 
         self.logger.info(f"Data parallel head {self.dp_head} and rank {self.dp_rank}")
 
-        current_sp_group = get_ulysses_sequence_parallel_group()
-        if current_sp_group is not None:
-            current_sp_group_ranks = dist.get_process_group_ranks(current_sp_group)
-            sp_group_ranks = dist.get_process_group_ranks(self.sp_group)
-            if current_sp_group_ranks != sp_group_ranks:
-                raise RuntimeError(
-                    f"Ulysses sequence parallel group mismatch: current ranks {current_sp_group_ranks} v.s. new ranks {sp_group_ranks}"
-                )
-        else:
-            set_ulysses_sequence_parallel_group(self.sp_group)
-
     def apply_tensor_parallel(self, device_mesh: DeviceMesh):
         try:
             num_attention_heads, num_key_value_heads = (
@@ -209,6 +198,9 @@ class FSDPEngine(BaseHFEngine):
             "layers.*.self_attn.q_proj": ColwiseParallel(),
             "layers.*.self_attn.k_proj": ColwiseParallel(),
             "layers.*.self_attn.v_proj": ColwiseParallel(),
+            # special q/k norm for qwen3
+            "layers.*.self_attn.k_norm": NoParallel(),
+            "layers.*.self_attn.q_norm": NoParallel(),
             # Reduce in RowwiseParallel, Scatter by Shard(1)
             "layers.*.self_attn.o_proj": RowwiseParallel(
                 output_layouts=Shard(1),
@@ -476,6 +468,7 @@ class FSDPEngine(BaseHFEngine):
         assert self.optimizer_config is not None
         assert self.lr_scheduler is not None
         assert self.fsdp_tp_device_mesh is not None
+        set_ulysses_sequence_parallel_group(self.sp_group)
 
         self.optimizer.zero_grad()
         mb_list = self.prepare_mb_list(input_)
@@ -488,7 +481,7 @@ class FSDPEngine(BaseHFEngine):
             .to(dtype=torch.float32, device=self.device)
         )
         assert total_loss_weight != 0
-        dist.all_reduce(total_loss_weight)
+        dist.all_reduce(total_loss_weight, group=self.dp_group)
 
         # Process microbatches with gradient accumulation
         for i, (pad_length, padded_mb_input, mb_input) in enumerate(
@@ -551,7 +544,7 @@ class FSDPEngine(BaseHFEngine):
 
         # NOTE: grad norm clip function is different
         grad_norm = fsdp2_clip_grad_norm(
-            self.model.parameters(),
+            list(self.model.parameters()),
             self.fsdp_tp_device_mesh,
             max_norm=self.optimizer_config.gradient_clipping,
         )
@@ -580,6 +573,7 @@ class FSDPEngine(BaseHFEngine):
         """Evaluate on a batch."""
         mb_list = self.prepare_mb_list(input_)
         mb_list = mb_list.to(self.device)
+        set_ulysses_sequence_parallel_group(self.sp_group)
 
         total_loss_weight = (
             sum([loss_weight_fn(mb) for mb in mb_list.mbs])
@@ -588,9 +582,9 @@ class FSDPEngine(BaseHFEngine):
             .to(dtype=torch.float32)
         )
         assert total_loss_weight != 0
+        dist.all_reduce(total_loss_weight, group=self.dp_group)
 
-        total_loss = 0.0
-        total_weight = 0.0
+        total_loss = torch.zeros(1, device=self.device, dtype=torch.float32)
 
         for pad_length, padded_mb_input, mb_input in zip(
             mb_list.padding_lengths, mb_list.padded_mbs, mb_list.mbs
@@ -644,10 +638,13 @@ class FSDPEngine(BaseHFEngine):
 
             # Simple weight calculation (could be improved)
             loss_scale = loss_weight_fn(mb_input) / total_loss_weight
-            total_loss += loss.item() * loss_scale
-            total_weight += loss_scale
+            # eval_batch does not run backward, the grad will not be averaged over DP group
+            # so we shouldn't multiple dp_size in loss_scale
+            total_loss += loss.clone().detach() * loss_scale
 
-        return torch.tensor(total_loss / total_weight)
+        dist.all_reduce(total_loss, group=self.dp_group)
+
+        return total_loss
 
     @torch.no_grad()
     def forward(
@@ -661,6 +658,7 @@ class FSDPEngine(BaseHFEngine):
         cu_seqlens = pack_tensor_dict(input_)["cu_seqlens"]
         mb_list = self.prepare_mb_list(input_)
         mb_list = mb_list.to(self.device)
+        set_ulysses_sequence_parallel_group(self.sp_group)
 
         if output_seqlens is None:
             output_seqlens = (cu_seqlens[1:] - cu_seqlens[:-1]).cpu().numpy().tolist()

areal/experimental/megatron_engine.py
@@ -3,7 +3,7 @@ import functools
 import gc
 import os
 from datetime import datetime
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Callable, Dict, List
 
 import mbridge
 import torch
@@ -33,7 +33,7 @@ from areal.experimental.utils.mcore.packed_context_parallel import (
     packed_context_parallel_forward,
 )
 from areal.experimental.utils.megatron_checkpointer import MegatronCheckpointManager
-from areal.utils import logging, name_resolve, names, stats_tracker
+from areal.utils import logging, name_resolve, names
 from areal.utils.data import (
     MicroBatchList,
     amend_position_ids,
@@ -367,7 +367,7 @@ class MegatronEngine(TrainEngine):
         path: str,
         tokenizer: Any | None = None,
         processor: Any | None = None,
-        base_model_path: Optional[str] = None,
+        base_model_path: str | None = None,
     ):
         assert self.model is not None, "Model is not initialized."
         os.makedirs(path, exist_ok=True)
@@ -494,6 +494,7 @@ class MegatronEngine(TrainEngine):
             .to(dtype=torch.float32)
         )
         assert total_loss_weight != 0
+        dist.all_reduce(total_loss_weight, group=mpu.get_data_parallel_group())
         max_total_len = max(m["cu_seqlens"][-1].item() for m in mb_list.padded_mbs)
         micro_batch_generator = iter(mb_list.padded_mbs)
         forward_step_count = 0
@@ -544,21 +545,6 @@ class MegatronEngine(TrainEngine):
         update_successful, grad_norm, _ = self.optimizer.step()
         current_lr = self.optimizer.param_groups[0]["lr"]
 
-        stats = [None]
-        if mpu.is_pipeline_last_stage():
-            stats = [stats_tracker.export(reduce_group=mpu.get_data_parallel_group())]
-        # broadcast stats to all ranks in context and model parallel group
-        dist.broadcast_object_list(
-            stats,
-            src=mpu.get_pipeline_model_parallel_last_rank(),
-            group=mpu.get_pipeline_model_parallel_group(),
-        )
-        stats = stats[0]
-        # TODO: currently if we export tracked stats here, stats will be exported twice.
-        # We need to: 1. Unify the way stats are tracked, through return value of `train_batch`
-        # or directly recorded by `stats_tracker`; 2. Find a way to properly handle stats broadcasting.
-        with stats_tracker.DEFAULT_TRACKER.disable_scope():
-            stats_tracker.scalar(**stats)
         return dict(
             update_successful=float(update_successful),
             grad_norm=float(grad_norm) if grad_norm is not None else float("nan"),
@@ -584,6 +570,7 @@ class MegatronEngine(TrainEngine):
             .to(dtype=torch.float32)
         )
         assert total_loss_weight != 0
+        dist.all_reduce(total_loss_weight, group=mpu.get_data_parallel_group())
         max_total_len = max(m["cu_seqlens"][-1].item() for m in mb_list.padded_mbs)
         micro_batch_generator = iter(mb_list.padded_mbs)
         forward_step_count = 0
@@ -612,7 +599,8 @@ class MegatronEngine(TrainEngine):
                 # automatically recorded by stats_tracker
                 loss = loss_fn(output, input_)
                 loss_scale = loss_weight_fn(input_) / total_loss_weight
-                loss_scale *= mpu.get_data_parallel_world_size()
+                # eval_batch does not run backward, the grad will not be averaged over DP group
+                # so we shouldn't multiple dp_size in loss_scale
                 loss *= loss_scale
                 return loss, {}
 
@@ -629,19 +617,6 @@ class MegatronEngine(TrainEngine):
             forward_only=True,
         )
 
-        # TODO: refactor the following lines into engine.sync_stats
-        stats = [None]
-        if mpu.is_pipeline_last_stage():
-            stats = [stats_tracker.export(reduce_group=mpu.get_data_parallel_group())]
-        # broadcast stats to all ranks in context and model parallel group
-        dist.broadcast_object_list(
-            stats,
-            src=mpu.get_pipeline_model_parallel_last_rank(),
-            group=mpu.get_pipeline_model_parallel_group(),
-        )
-        stats = stats[0]
-        with stats_tracker.DEFAULT_TRACKER.disable_scope():
-            stats_tracker.scalar(**stats)
         return None
 
     @torch.no_grad()

areal/utils/fsdp.py
@@ -1,4 +1,6 @@
 import math
+from collections import defaultdict
+from functools import partial
 from typing import List
 
 import torch
@@ -6,7 +8,9 @@ import torch.distributed as dist
 import torch.nn as nn
 from torch.distributed import ProcessGroup
 from torch.distributed.device_mesh import DeviceMesh, init_device_mesh
-from torch.distributed.tensor import DTensor
+from torch.distributed.tensor import DeviceMesh, DTensor, Replicate, distribute_module
+from torch.distributed.tensor.parallel import ParallelStyle
+from torch.distributed.tensor.placement_types import Placement
 from transformers import PreTrainedModel
 
 from areal.platforms import current_platform
@@ -69,10 +73,65 @@ __all__ = [
     "apply_fsdp2",
     "fsdp2_load_full_state_dict",
     "get_cosine_schedule_with_warmup",
+    "NoParallel",
 ]
 
 
 logger = logging.getLogger("FSDPEngine")
+
+
+# Copied from torchtitan. Used for Qwen3 Q/K norm.
+# NOTE: This is to achieve replicate computation on the gate module in the MoE router.
+# It does nothing other than (1) setting the module parameters as DTensors on the given mesh
+# and (2) inserting hooks to module boundary to change torch.Tensor to DTensor and back.
+# The reason we need this wrapping is to ensure all parameters are on the same 1D/2D mesh,
+# which is assumed by (1) gradient norm clipping, and (2) optimizer fused implementation.
+class NoParallel(ParallelStyle):
+    def __init__(
+        self,
+        *,
+        input_layout: Placement | None = None,
+        output_layout: Placement | None = None,
+        use_local_output: bool = True,
+    ):
+        super().__init__()
+        self.input_layout = input_layout or Replicate()
+        self.output_layout = output_layout or Replicate()
+        self.desired_input_layout = Replicate()
+        self.use_local_output = use_local_output
+
+    @staticmethod
+    def _prepare_input_fn(input_layout, desired_input_layout, mod, inputs, device_mesh):
+        # annotate module input placements/sharding with input_layouts
+        input_tensor = inputs[0]
+        if not isinstance(input_tensor, DTensor):
+            input_tensor = DTensor.from_local(
+                input_tensor, device_mesh, (input_layout,), run_check=False
+            )
+
+        if input_layout != desired_input_layout:
+            input_tensor = input_tensor.redistribute(
+                placements=(desired_input_layout,), async_op=True
+            )
+        return (input_tensor, *inputs[1:])
+
+    @staticmethod
+    def _prepare_output_fn(output_layout, use_local_output, mod, outputs, device_mesh):
+        if outputs.placements != (output_layout,):
+            outputs = outputs.redistribute(placements=(output_layout,), async_op=True)
+        # back to local tensor
+        return outputs.to_local() if use_local_output else outputs
+
+    def _apply(self, module: nn.Module, device_mesh: DeviceMesh) -> nn.Module:
+        return distribute_module(
+            module,
+            device_mesh,
+            None,
+            partial(
+                self._prepare_input_fn, self.input_layout, self.desired_input_layout
+            ),
+            partial(self._prepare_output_fn, self.output_layout, self.use_local_output),
+        )
 
 
 def to_local_if_dtensor(tensor: torch.Tensor | DTensor) -> torch.Tensor:
@@ -185,22 +244,40 @@ def clip_grad_by_total_norm_fp32(
     max_norm: int | float,
     total_norm: float,
 ):
-    params = []
-    grads = []
+    # dtype -> grad
+    grads = defaultdict(list)
     for param in parameters:
         if param.grad is not None:
-            assert param.grad.type() == "torch.cuda.FloatTensor"
-            params.append(param)
-            grads.append(to_local_if_dtensor(param.grad).detach())
+            # For naive FSDP, lm_head has bf16 grad while others have fp32 grad
+            grad = to_local_if_dtensor(param.grad).detach()
+            grads[grad.dtype].append(grad)
 
+    assert len(grads) > 0, len(grads)
     clip_coeff = max_norm / (total_norm + 1.0e-6)
     if clip_coeff < 1.0:
-        dummy_overflow_buf = torch.tensor(
-            [0], dtype=torch.int, device=current_platform.device_type
-        )
-        multi_tensor_applier(
-            multi_tensor_scale_impl, dummy_overflow_buf, [grads, grads], clip_coeff
-        )
+        for dtype, _grads in grads.items():
+            dummy_overflow_buf = torch.tensor(
+                [0], dtype=torch.int, device=current_platform.device_type
+            )
+            if dtype == torch.float32:
+                multi_tensor_applier(
+                    multi_tensor_scale_impl,
+                    dummy_overflow_buf,
+                    [_grads, _grads],
+                    clip_coeff,
+                )
+            else:
+                from .multi_tensor_apply import (
+                    local_multi_tensor_applier,
+                    local_multi_tensor_scale,
+                )
+
+                local_multi_tensor_applier(
+                    local_multi_tensor_scale,
+                    dummy_overflow_buf,
+                    [_grads, _grads],
+                    clip_coeff,
+                )
 
 
 def fsdp2_clip_grad_norm(

areal/utils/recover.py
@@ -2,7 +2,7 @@ import dataclasses
 import json
 import os
 import pickle
-from typing import Dict, List
+from typing import TYPE_CHECKING, Dict, List
 
 import torch.distributed as dist
 from torchdata.stateful_dataloader import StatefulDataLoader
@@ -15,7 +15,9 @@ from areal.platforms import current_platform
 from areal.utils import logging, timeutil
 from areal.utils.evaluator import Evaluator
 from areal.utils.saver import Saver
-from areal.utils.stats_logger import StatsLogger
+
+if TYPE_CHECKING:
+    from areal.utils.stats_logger import StatsLogger
 
 logger = logging.getLogger("recover")
 
@@ -168,7 +170,7 @@ class RecoverHandler:
         step_info: StepInfo,
         saver: Saver,
         evaluator: Evaluator,
-        stats_logger: StatsLogger,
+        stats_logger: "StatsLogger",
         dataloader: StatefulDataLoader,
         tokenizer: PreTrainedTokenizerFast | None = None,
         processor: AutoProcessor | None = None,
@@ -215,7 +217,7 @@ class RecoverHandler:
         engine: TrainEngine | Dict[str, TrainEngine],
         saver: Saver,
         evaluator: Evaluator,
-        stats_logger: StatsLogger,
+        stats_logger: "StatsLogger",
         dataloader: StatefulDataLoader,
         inference_engine: InferenceEngine | None = None,
         weight_update_meta: WeightUpdateMeta | None = None,

examples/experimental/gsm8k_sft_megatron.py
@@ -1,7 +1,10 @@
 import os
 import sys
 
+import torch
+import torch.distributed as dist
 from megatron.core import parallel_state as mpu
+from tensordict import TensorDict
 from torchdata.stateful_dataloader import StatefulDataLoader
 
 from areal.api.alloc_mode import AllocationMode
@@ -117,6 +120,8 @@ def main(args):
                 steps_per_epoch=len(train_dataloader),
             )
             # NOTE: data are identical across model+context parallel group
+            data: TensorDict
+            data = data.to(torch.cuda.current_device())
             data = broadcast_tensor_container(
                 data,
                 src_rank=engine.current_data_parallel_head(),
@@ -132,20 +137,6 @@ def main(args):
             with stats_tracker.record_timing("save"):
                 saver.save(engine, epoch, step, global_step, tokenizer=tokenizer)
 
-            with stats_tracker.record_timing("eval"):
-
-                def evaluate_fn():
-                    with stats_tracker.scope("sft-eval"):
-                        for data in valid_dataloader:
-                            engine.evaluate_lm(data)
-
-                evaluator.evaluate(
-                    evaluate_fn,
-                    epoch,
-                    step,
-                    global_step,
-                )
-
             with stats_tracker.record_timing("checkpoint_for_recover"):
                 recover_handler.dump(
                     engine,
@@ -156,6 +147,33 @@ def main(args):
                     train_dataloader,
                     tokenizer=tokenizer,
                 )
+
+            dist.barrier(device_ids=[engine.device.index])
+            torch.cuda.synchronize()
+
+            with stats_tracker.record_timing("eval"):
+                # No need to log anything. Logging will be handled outside
+                # via stats_tracker.export().
+                def evaluate_fn():
+                    with stats_tracker.scope("sft-eval"):
+                        for data in valid_dataloader:
+                            data = data.to(torch.cuda.current_device())
+                            data = broadcast_tensor_container(
+                                data,
+                                src_rank=engine.current_data_parallel_head(),
+                                group=engine.context_and_model_parallel_group,
+                            )
+                            engine.evaluate_lm(data)
+
+                evaluator.evaluate(
+                    evaluate_fn,
+                    epoch,
+                    step,
+                    global_step,
+                )
+
+            dist.barrier(device_ids=[engine.device.index])
+            torch.cuda.synchronize()
 
             stats.update(
                 stats_tracker.export_all(reduce_group=mpu.get_data_parallel_group())

examples/vlm/clevr_count_70k_sft.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+import torch
+import torch.distributed as dist
+from tensordict import TensorDict
 from torchdata.stateful_dataloader import StatefulDataLoader
 
 from areal.api.alloc_mode import AllocationMode
@@ -113,6 +116,8 @@ def main_sft():
             )
 
             # NOTE: data are identical across model+context parallel group
+            data: TensorDict
+            data = data.to(torch.cuda.current_device())
             data = broadcast_tensor_container(
                 data,
                 src_rank=engine.current_data_parallel_head(),
@@ -137,21 +142,6 @@ def main_sft():
                     processor=processor,
                 )
 
-            with stats_tracker.record_timing("eval"):
-                # No need to log anything. Logging will be handled outside
-                # via stats_tracker.export().
-                def evaluate_fn():
-                    with stats_tracker.scope("sft-eval"):
-                        for data in valid_dataloader:
-                            engine.evaluate_lm(data)
-
-                evaluator.evaluate(
-                    evaluate_fn,
-                    epoch,
-                    step,
-                    global_step,
-                )
-
             with stats_tracker.record_timing("checkpoint_for_recover"):
                 recover_handler.dump(
                     engine,
@@ -163,6 +153,33 @@ def main_sft():
                     tokenizer=tokenizer,
                     processor=processor,
                 )
+
+            dist.barrier(device_ids=[engine.device.index])
+            torch.cuda.synchronize()
+
+            with stats_tracker.record_timing("eval"):
+                # No need to log anything. Logging will be handled outside
+                # via stats_tracker.export().
+                def evaluate_fn():
+                    with stats_tracker.scope("sft-eval"):
+                        for data in valid_dataloader:
+                            data = data.to(torch.cuda.current_device())
+                            data = broadcast_tensor_container(
+                                data,
+                                src_rank=engine.current_data_parallel_head(),
+                                group=engine.context_and_model_parallel_group,
+                            )
+                            engine.evaluate_lm(data)
+
+                evaluator.evaluate(
+                    evaluate_fn,
+                    epoch,
+                    step,
+                    global_step,
+                )
+
+            dist.barrier(device_ids=[engine.device.index])
+            torch.cuda.synchronize()
 
             stats_logger.commit(
                 epoch,