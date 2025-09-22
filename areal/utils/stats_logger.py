4141df54b7c8361b029b1e226f788d1d4ace1c44
MayDomine <57244158+MayDomine@users.noreply.github.com>
2025-09-22 11:18:43 +0800

    Add swanlab log for StatsLogger (#362)
    * add swanlab log
    
    * fix not define and swanlab.mode
    
    * fix the format issue
    
    ---------
    
    Co-authored-by: Wei Fu <36355462+garrett4wade@users.noreply.github.com>

--- areal/utils/stats_logger.py
+++ areal/utils/stats_logger.py
@@ -3,6 +3,7 @@ import os
 import time
 from typing import Dict, List
 
+import swanlab
 import torch.distributed as dist
 import wandb
 from megatron.core import parallel_state as mpu
@@ -50,6 +51,22 @@ class StatsLogger:
             resume="allow",
             settings=wandb.Settings(start_method="fork"),
         )
+
+        swanlab_config = self.config.swanlab
+        if swanlab_config.mode != "disabled":
+            if swanlab_config.api_key:
+                swanlab.login(swanlab_config.api_key)
+            else:
+                swanlab.login()
+
+        swanlab_config = self.config.swanlab
+        swanlab.init(
+            project=swanlab_config.project or self.config.experiment_name,
+            experiment_name=swanlab_config.name or self.config.trial_name + "_train",
+            config=swanlab_config.config,
+            logdir=self.get_log_path(self.config),
+            mode=swanlab_config.mode,
+        )
         # tensorboard logging
         self.summary_writer = None
         if self.config.tensorboard.path is not None:
@@ -70,6 +87,7 @@ class StatsLogger:
             f"Training completes! Total time elapsed {time.monotonic() - self.start_time:.2f}."
         )
         wandb.finish()
+        swanlab.finish()
         if self.summary_writer is not None:
             self.summary_writer.close()
 
@@ -98,6 +116,7 @@ class StatsLogger:
             logger.info(f"Stats ({i+1}/{len(data)}):")
             self.print_stats(item)
             wandb.log(item, step=log_step + i)
+            swanlab.log(item, step=log_step + i)
             if self.summary_writer is not None:
                 for key, val in item.items():
                     self.summary_writer.add_scalar(f"{key}", val, log_step + i)
