ae2f2e83582e41963530d55cff8a51258b058984
Yifeng Wang（正经人王同学） <86822589+zjrwtx@users.noreply.github.com>
2025-10-11 18:10:13 +0800

    upgrade sglang version requirement (#428)
    * fix launch_server.py: error: unrecognized arguments: --max-loaded-loras 1

--- examples/env/setup-pip-deps.sh
+++ examples/env/setup-pip-deps.sh
@@ -4,7 +4,7 @@ pip install -U pip
 pip uninstall pynvml cugraph-dgl dask-cuda cugraph-service-server raft-dask cugraph cuml cugraph-pyg -y
 pip install torch==2.7.1 torchaudio==2.7.1 torchvision==0.22.1 "deepspeed>=0.17.2" pynvml
 pip install flashinfer-python==0.2.7.post1 --no-build-isolation
-pip install "sglang[all]==0.4.9.post2" 
+pip install "sglang[all]>=0.5.2"
 pip install megatron-core==0.11.0 nvidia-ml-py
 pip install git+https://github.com/garrett4wade/cugae --no-build-isolation --verbose
 pip install "flash-attn<=2.8.2" --no-build-isolation
